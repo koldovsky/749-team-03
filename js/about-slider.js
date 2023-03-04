e204d59cdf2d7a650aa1d06162fd2f0dfef88d90
hopeei <gopeydanylo@gmail.com>
2023-03-04 15:45:46 +0200

    finish two block

--- js/about-slider.js
+++ js/about-slider.js
@@ -1,26 +1,26 @@
-(function(){
- 
-    const elements = [
-        '<div>Detailed</div>',
-        '<div>Statistics</div>',
-        '<div>About</div>',
-        '<div>Drones</div>'
-    ];
-      
-       let currentSlide = 0;
-    
-       function renderSlides(elements) {
-         const slidersContainer = document.querySelector('.about-carousel__slides');
-         slidersContainer.innerHTML = elements[currentSlide];
-       } 
-    
-       function nextSlide() {
-        currentSlide = currentSlide + 1 >= elements.length ? 0 : currentSlide + 1;
-        renderSlides(elements);
-       }
-    
-       setInterval(nextSlide, 2000);
-    
-       renderSlides(elements);
-    
-    })();
+(function () {
+
+  const elements = [
+    '<div>Detailed</div>',
+    '<div>Statistics</div>',
+    '<div>About</div>',
+    '<div>Drones</div>'
+  ];
+
+  let currentSlide = 0;
+
+  function renderSlides(elements) {
+    const slidersContainer = document.querySelector('.about-carousel__slides');
+    slidersContainer.innerHTML = elements[currentSlide];
+  }
+
+  function nextSlide() {
+    currentSlide = currentSlide + 1 >= elements.length ? 0 : currentSlide + 1;
+    renderSlides(elements);
+  }
+
+  setInterval(nextSlide, 2000);
+
+  renderSlides(elements);
+
+})();
